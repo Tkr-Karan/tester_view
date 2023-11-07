23ff76577596da931fe229fd50237732d2ca59f8
Tkr-karan <karankumar072508@gmail.com>
2023-11-07 15:51:22 +0530

    block test code refactoring

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,13 @@
 // configure the store
 import { configureStore } from "@reduxjs/toolkit";
 import testerSlice from "./testerSlice";
+import blockSlice from "./blockSlice";
 
 const store = configureStore({
   reducer: {
-    tester : testerSlice.reducer,
+    tester: testerSlice.reducer,
+    block: blockSlice.reducer,
   },
 });
-
 
 export default store;
